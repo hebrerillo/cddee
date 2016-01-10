ed20839fa401536274a2cb9f064b15456bac33a9
Jesus <hebrerillo@gmail.com>
2016-01-10 22:21:48 +0100

    Testing

--- main.c
+++ main.c
@@ -6,25 +6,23 @@
 
 int main(int argc, char** argv)
 {
-    List l, l2;
+    List l, l2,l3;
 
     int n[] = {1, 2, 3};
     int n2[] = {10, 11, 12};
     
 
     //init lists
-    init(&l, destroySimpleList, printListInt);
-    init(&l2, destroySimpleList, printListInt);
+    init(&l, NULL, NULL);
+//    init(&l2, NULL, printListInt);
 
 
     //insert data
     insert(&l, n, sizeof (n));
     insert(&l, n2, sizeof (n2));
     
-    printList(&l);
-    pop(&l);
-    printList(&l);
-
     destroyList(&l);
+//    destroyList(&l2);
+    
     return (EXIT_SUCCESS);
 }
